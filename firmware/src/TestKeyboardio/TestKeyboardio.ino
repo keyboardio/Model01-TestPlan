37e95e1365071fe0158bd27cd6fb8db3ab5cacb5
Jesse Vincent <jesse@bestpractical.com>
2015-02-09 23:04:39 -0800

    Switch to the "correct" i2c address for the Model 01

--- firmware/src/TestKeyboardio/TestKeyboardio.ino
+++ firmware/src/TestKeyboardio/TestKeyboardio.ino
@@ -4,9 +4,9 @@
 
 // Uncomment one of the four lines to match your SX1509's address
 //  pin selects. SX1509 breakout defaults to [0:0] (0x3E).
-const byte SX1509_ADDRESS = 0x3E;  // SX1509 I2C address (00)
+//const byte SX1509_ADDRESS = 0x3E;  // SX1509 I2C address (00)
 //const byte SX1509_ADDRESS = 0x3F;  // SX1509 I2C address (01)
-//const byte SX1509_ADDRESS = 0x70;  // SX1509 I2C address (10)
+const byte SX1509_ADDRESS = 0x70;  // SX1509 I2C address (10)
 //const byte SX1509_ADDRESS = 0x71;  // SX1509 I2C address (11)
 sx1509Class sx1509(SX1509_ADDRESS);
 
